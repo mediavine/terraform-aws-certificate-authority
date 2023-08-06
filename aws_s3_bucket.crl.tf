2010d912e1534542a84b867940de89c3469a6238
Jocelyn Aladin <aladinjocelyn@gmail.com>
2023-08-06 18:07:37 -0400

    Update aws_s3_bucket.crl.tf

--- aws_s3_bucket.crl.tf
+++ aws_s3_bucket.crl.tf
@@ -29,9 +29,9 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "crl" {
 
   rule {
     apply_server_side_encryption_by_default {
-      # kms_master_key_id = var.kms_key.arn
-      # sse_algorithm     = "aws:kms"
-      sse_algorithm     = "AES256"
+      kms_master_key_id = var.kms_key.arn
+      sse_algorithm     = "aws:kms"
+      # sse_algorithm     = "AES256"
     }
   }
 }
